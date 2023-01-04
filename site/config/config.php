05dab282271adf9f8051715de2f962f5596f62da
Martin Schneider <martin.schneider@pooliestudios.com>
2023-01-04 14:39:39 +0100

    bringt das Backend in Form, initiales Styling

--- site/config/config.php
+++ site/config/config.php
@@ -1,4 +1,21 @@
 <?php
 return [
-    'debug'  => true
+    'debug'  => true,
+    'routes' => [
+        [
+            'pattern' => '(:any)',
+            'action'  => function($uid) {
+                $page = page($uid);
+                if(!$page) $page = page('blog/' . $uid);
+                if(!$page) $page = site()->errorPage();
+                return site()->visit($page);
+            }
+        ],
+        [
+            'pattern' => 'blog/(:any)',
+            'action'  => function($uid) {
+                go($uid);
+            }
+        ]
+    ]
 ];
