81e8bd562db5dc87b8a9a6750c64eec18db0f599
Martin Schneider <martin.schneider@pooliestudios.com>
2023-01-05 19:05:01 +0100

    Artikel-Styling und Import

--- site/config/config.php
+++ site/config/config.php
@@ -1,13 +1,14 @@
 <?php
 return [
     'debug'  => true,
+    'locale' => 'de_DE.utf-8',
     'routes' => [
         [
             'pattern' => '(:any)',
             'action'  => function($uid) {
                 $page = page($uid);
-                if(!$page) $page = page('blog/' . $uid);
-                if(!$page) $page = site()->errorPage();
+                if (!$page) $page = page('blog/' . $uid);
+                if (!$page) $page = site()->errorPage();
                 return site()->visit($page);
             }
         ],
