4f87f041dc2efdf17fc75983b356d1c9f76f3a25
Martin Schneider <martin.schneider@pooliestudios.com>
2023-01-19 16:24:40 +0100

    korrektes Pattern für Blog-URLs

--- site/config/config.php
+++ site/config/config.php
@@ -8,18 +8,18 @@ return [
     ],
     'routes' => [
         [
-            'pattern' => '(:any)',
-            'action'  => function($uid) {
-                $page = page($uid);
-                if(!$page) $page = page('blog/' . $uid);
+            'pattern' => '(:num)/(:num)/(:num)/(:any)',
+            'action'  => function($year, $month, $day, $slug) {
+                $page = page($year . '/' . $month . '/' . $day  . '/' . $slug);
+                if(!$page) $page = page('blog/' . $year . '/' . $month . '/' . $day  . '/' . $slug);
                 if(!$page) $page = site()->errorPage();
                 return site()->visit($page);
-            }
+            },
         ],
         [
-            'pattern' => 'blog/(:any)',
-            'action'  => function($uid) {
-                go($uid);
+            'pattern' => 'blog/(:num)/(:num)/(:num)/(:any)',
+            'action'  => function($year, $month, $day, $slug) {
+                go($year . '/' . $month . '/' . $day  . '/' . $slug);
             }
         ],
         [
