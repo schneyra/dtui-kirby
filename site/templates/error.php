d79b874d5b108dbbf3e60c288404d15037e51231
Martin Schneider <martin.schneider@pooliestudios.com>
2023-02-02 14:24:36 +0100

    Fehlerseite

--- site/templates/error.php
+++ site/templates/error.php
@@ -1,3 +1,8 @@
 <?php snippet('site-header') ?>
-    <h1>Oh noez!</h1>
+
+<div class="article">
+  <h1>Oh noez!</h1>
+  <p>Hier wurde nichts gefunden. Dies ist ein Fehler <em>404</em>.</p>
+</div>
+
 <?php snippet('site-footer') ?>
