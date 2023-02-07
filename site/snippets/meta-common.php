89a5c3214cf0c31d45f711909f639899d124d0a3
Martin Schneider <martin.schneider@pooliestudios.com>
2023-02-07 10:23:16 +0100

    Standard-OG-Kram

--- site/snippets/meta-common.php
+++ site/snippets/meta-common.php
@@ -1,20 +1,16 @@
-<?php /*<meta name="description" content="<?= $description ?>" />
-<link rel="canonical" href="<?= $page->articleUrl() ?>" />
+<?php $description = "„der tag und ich“ ist das private Blog von Martin Schneider aus Bonn. Hier geht es um alltägliche Belanglosigkeiten, Fotografie, Musik, Reisen, Webentwicklung und was mir sonst so einfällt." ?>
+
+<meta name="description" content="<?= $description ?>" />
+<link rel="canonical" href="<?= $page->url() ?>" />
+
 <meta property="og:locale" content="de_DE" />
 <meta property="og:type" content="article" />
 <meta property="og:title" content="<?= $page->title() ?>" />
-<meta property="og:description" content="<?= $description ?>" />
-<meta property="og:url" content="<?= $page->articleUrl() ?>" />
+<meta property="og:description" content="" />
+<meta property="og:url" content="<?= $page->url() ?>" />
 <meta property="og:site_name" content="Martin Schneider" />
-<meta property="article:publisher" content="schneyra" />
-<meta property="article:author" content="schneyra" />
-<meta property="article:published_time" content="<?= $page->date()->strtotime() ?>" />
-<meta property="article:modified_time" content="<?= $page->modified('YYYY-MM-dd HH:mm:ss') ?>" />
+
 <meta name="author" content="Martin Schneider" />
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:creator" content="@schneyra" />
 <meta name="twitter:site" content="@schneyra" />
-<meta name="twitter:label1" content="Autor" />
-<meta name="twitter:data1" content="Martin Schneider" />
-<meta name="twitter:label2" content="Lesezeit" />
-<meta name="twitter:data2" content="<?= $readingTime['minutes'] ?> Minuten" />*/ ?>
