d5ceb58ced2e85e6e593a3f13984cfa00c2826d2
Martin Schneider <martin.schneider@pooliestudios.com>
2023-02-27 10:31:15 +0100

    Update OG-Image

--- site/templates/article.png.php
+++ site/templates/article.png.php
@@ -10,19 +10,19 @@
 $kirby->response()->type('image/png');
 
 // Define canvas size
-$canvas = imagecreatetruecolor(1200, 628);
+$canvas = imagecreatetruecolor(2400, 1200);
 
 // Define colors
-$colorYellow      = imagecolorallocate($canvas, 255, 204, 0);
-$backgroundColor  = imagecolorallocate($canvas, 255, 255, 255);
-$titleColor       = imagecolorallocate($canvas, 51, 51, 51);
-$siteTitleColor   = imagecolorallocate($canvas, 17, 50, 100);
+$colorYellow      = imagecolorallocate($canvas, 212, 173, 33);
+$backgroundColor  = imagecolorallocate($canvas, 249, 249, 251);
+$titleColor       = imagecolorallocate($canvas, 22, 26, 34);
+$siteTitleColor   = imagecolorallocate($canvas, 25,47,77);
 
 // Set background
 imagefill($canvas, 0, 0, $backgroundColor);
 
 // Draw rectangle
-imagefilledrectangle($canvas, 725, 560, 1130, 565, $colorYellow);
+imagefilledrectangle($canvas, 1450, 1120, 2260, 1130, $colorYellow);
 
 // Path to .ttf font file
 $fontFile = './assets/fonts/Vollkorn-Bold.ttf';
@@ -30,8 +30,8 @@ $fontFile = './assets/fonts/Vollkorn-Bold.ttf';
 // Write page title to canvas
 $title  = $page->title()->toString();
 $title  = wordwrap($title, 30);
-imagefttext($canvas, 45, 0, 100, 150, $titleColor, $fontFile, $title);
-imagefttext($canvas, 35, 0, 725, 550, $siteTitleColor, $fontFile, strtoupper($site->title()));
+imagefttext($canvas, 90, 0, 200, 300, $titleColor, $fontFile, $title);
+imagefttext($canvas, 70, 0, 1450, 1100, $siteTitleColor, $fontFile, strtoupper($site->title()));
 
 // Place logo in the corner
 //$logoFile = './assets/kirby-logo.png';
